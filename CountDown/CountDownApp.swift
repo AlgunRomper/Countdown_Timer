9d2ad8fc2f890443b5aff0d47e54360058589b18
KaterinaStavrova <katerina.stavrova@stepadev.com>
2024-05-01 12:52:07 +0100

    Initial Commit

---
+++ CountDown/CountDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountDownApp.swift
+//  CountDown
+//
+//  Created by Algun Romper on 1/5/24.
+//
+
+import SwiftUI
+
+@main
+struct CountDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
